d95b2fc3bd2e40a821928bc96d6c667b89b81fe8
Yebin Chon <yebin.chon@princeton.edu>
2024-03-21 19:09:02 -0400

    bugfix

--- src/core/parallelizer/src/Pass.cpp
+++ src/core/parallelizer/src/Pass.cpp
@@ -19,6 +19,7 @@
  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#include "MemSpec.hpp"
 #include "Parallelizer.hpp"
 
 namespace arcana::gino {
@@ -26,31 +27,23 @@ namespace arcana::gino {
 /*
  * Options of the Parallelizer pass.
  */
-static cl::opt<bool> ForceParallelization(
-    "noelle-parallelizer-force",
-    cl::ZeroOrMore,
-    cl::Hidden,
-    cl::desc("Force the parallelization"));
-static cl::opt<bool> ForceNoSCCPartition(
-    "dswp-no-scc-merge",
-    cl::ZeroOrMore,
-    cl::Hidden,
-    cl::desc("Force no SCC merging when parallelizing"));
-static cl::list<int> LoopIndexesWhiteList(
-    "noelle-loops-white-list",
-    cl::ZeroOrMore,
-    cl::CommaSeparated,
-    cl::desc("Parallelize only a subset of loops"));
-static cl::list<int> LoopIndexesBlackList(
-    "noelle-loops-black-list",
-    cl::ZeroOrMore,
-    cl::CommaSeparated,
-    cl::desc("Don't parallelize a subset of loops"));
+static cl::opt<bool>
+    ForceParallelization("noelle-parallelizer-force", cl::ZeroOrMore,
+                         cl::Hidden, cl::desc("Force the parallelization"));
+static cl::opt<bool>
+    ForceNoSCCPartition("dswp-no-scc-merge", cl::ZeroOrMore, cl::Hidden,
+                        cl::desc("Force no SCC merging when parallelizing"));
+static cl::list<int>
+    LoopIndexesWhiteList("noelle-loops-white-list", cl::ZeroOrMore,
+                         cl::CommaSeparated,
+                         cl::desc("Parallelize only a subset of loops"));
+static cl::list<int>
+    LoopIndexesBlackList("noelle-loops-black-list", cl::ZeroOrMore,
+                         cl::CommaSeparated,
+                         cl::desc("Don't parallelize a subset of loops"));
 
 Parallelizer::Parallelizer()
-  : ModulePass{ ID },
-    forceParallelization{ false },
-    forceNoSCCPartition{ false } {
+    : ModulePass{ID}, forceParallelization{false}, forceNoSCCPartition{false} {
 
   return;
 }
@@ -71,12 +64,15 @@ bool Parallelizer::runOnModule(Module &M) {
    * Fetch the outputs of the passes we rely on.
    */
   auto &noelle = getAnalysis<Noelle>();
+  MemorySpeculationOracle memSpecOracle(noelle, M);
+  noelle.addAnalysis(&memSpecOracle);
+  bool readProfile = memSpecOracle.profileExists();
   auto heuristics = getAnalysis<HeuristicsPass>().getHeuristics(noelle);
 
   /*
    * Parallelize the loops of the target program.
    */
-  auto modified = this->parallelizeLoops(noelle, heuristics);
+  auto modified = this->parallelizeLoops(noelle, heuristics, readProfile);
 
   return modified;
 }
@@ -94,23 +90,22 @@ void Parallelizer::getAnalysisUsage(AnalysisUsage &AU) const {
 
 // Next there is code to register your pass to "opt"
 char arcana::gino::Parallelizer::ID = 0;
-static RegisterPass<arcana::gino::Parallelizer> X(
-    "parallelizer",
-    "Automatic parallelization of sequential code");
+static RegisterPass<arcana::gino::Parallelizer>
+    X("parallelizer", "Automatic parallelization of sequential code");
 
 // Next there is code to register your pass to "clang"
 static arcana::gino::Parallelizer *_PassMaker = NULL;
-static RegisterStandardPasses _RegPass1(
-    PassManagerBuilder::EP_OptimizerLast,
-    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
-      if (!_PassMaker) {
-        PM.add(_PassMaker = new arcana::gino::Parallelizer());
-      }
-    }); // ** for -Ox
-static RegisterStandardPasses _RegPass2(
-    PassManagerBuilder::EP_EnabledOnOptLevel0,
-    [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
-      if (!_PassMaker) {
-        PM.add(_PassMaker = new arcana::gino::Parallelizer());
-      }
-    }); // ** for -O0
+static RegisterStandardPasses
+    _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+              [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+                if (!_PassMaker) {
+                  PM.add(_PassMaker = new arcana::gino::Parallelizer());
+                }
+              }); // ** for -Ox
+static RegisterStandardPasses
+    _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+              [](const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+                if (!_PassMaker) {
+                  PM.add(_PassMaker = new arcana::gino::Parallelizer());
+                }
+              }); // ** for -O0
